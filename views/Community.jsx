a1ec9234ad20f6967ef3369910bf86919dd07f4a
CarkX55 <viralicvids55@gmail.com>
2026-05-03 19:20:27 +0100

    Delete views directory

--- views/Community.jsx
+++
@@ -1,8 +0,0 @@
-export default function Community() {
-  return (
-    <div className="space-y-4">
-      <h2 className="text-xl text-grimorio-gold">Comunidad</h2>
-      <p>Conecta con otros constructores</p>
-    </div>
-  );
-}
