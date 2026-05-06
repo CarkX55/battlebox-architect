57f49bbce918ce26b2e6362322fb72c19f28baa2
CarkX55 <viralicvids55@gmail.com>
2026-05-06 18:56:08 +0100

    Add files via upload

--- src/store/useAppStore.js
+++ src/store/useAppStore.js
@@ -4,8 +4,12 @@ export const useAppStore = create((set) => ({
   currentView: 'Home',
   activeDeck: null,
   selectedDecks: [],
+  isDbLoading: false,
+  loadingProgress: 0,
   setCurrentView: (view) => set({ currentView: view }),
   setActiveDeck: (deck) => set({ activeDeck: deck }),
+  setDbLoading: (loading) => set({ isDbLoading: loading }),
+  setLoadingProgress: (progress) => set({ loadingProgress: progress }),
   toggleDeckSelection: (deckId) => set((state) => ({
     selectedDecks: state.selectedDecks.includes(deckId)
       ? state.selectedDecks.filter(id => id !== deckId)
